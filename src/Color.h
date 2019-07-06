65e90f271087c35a0c15e5ab9e0ec736df26a091
Prez <10709507+perezite@users.noreply.github.com>
2019-07-06 21:46:43 +0200

    Extracting classes wip

--- src/Color.h
+++ src/Color.h
@@ -8,6 +8,10 @@ namespace sb
 			: r(r_), g(g_), b(b_), a(a_)
 		{ }
 
+		static sb::Color createFromRGB(int r, int g = 255, int b = 255, int a = 255) {
+			return sb::Color(r / 255.f, g / 255.f, b / 255.f, a / 255.f);
+		}
+
 		float r, g, b, a;
 	};
 }
