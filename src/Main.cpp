6c7904c8dcb062226aa6ba95540d439d50eb52de
Prez <simulo2000@gmail.com>
2022-03-11 16:57:34 +0100

    Cleanup

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,17 +1,23 @@
-#include "CameraDemo1.h"
-#include "Game.h"
+#include "Scene.h"
 #include "Logger.h"
+#include "CameraDemo1.h"
 #include "SceneDemo1.h"
 #include "TweenDemo1.h"
 
 // TODO
-// Create final tween implementation file with demo
+// Simplify collision detection
 
 using namespace blocks;
 
+void runGame() {
+	//Scene scene(4, 4);
+	Scene scene(10, 18);
+	scene.run();
+}
+
 int main() {
 	//tweenDemo1::run();
-	blocks::runGame();
+	runGame();
 
 	return 0;
 }
