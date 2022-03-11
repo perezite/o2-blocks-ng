f65215a5b4823b1ac12b1a7b8f192191b5f5deed
Prez <simulo2000@gmail.com>
2022-03-11 16:48:48 +0100

    Cleanup

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,14 +5,13 @@
 #include "TweenDemo1.h"
 
 // TODO
-// Implement fluent syntax for tween setters in the particle system
 // Create final tween implementation file with demo
 
 using namespace blocks;
 
 int main() {
-	tweenDemo1::run();
-	//blocks::runGame();
+	//tweenDemo1::run();
+	blocks::runGame();
 
 	return 0;
 }
