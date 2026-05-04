dde0308e48878fb052b8748d64e76068a721d6cc
perezite <simulo2000@gmail.com>
2026-05-04 23:34:54 +0200

    Add SDL3 and basic CMakeLists.txt

---
+++ Source/Main.cpp
@@ -0,0 +1,39 @@
+﻿#include <SDL3/SDL.h>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow(
+        "Simple Color Change",
+        800, 600,
+        SDL_WINDOW_RESIZABLE
+    );
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                running = false;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 128, 128, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+
+        SDL_Delay(10);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
